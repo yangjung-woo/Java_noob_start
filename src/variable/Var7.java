e3baf9f109e9d72fe7c11d09fde2a282081ca532
양정우 <yee777222000@gmaill.com>
2023-12-20 21:53:55 +0900

    Java 김영한

---
+++ src/variable/Var7.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        //다양한 자료형
+        int a = 10;
+        double b = 10.5;
+        boolean c = true;
+        char d = 'C';
+        String e ="문자열";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+
+    }
+}
